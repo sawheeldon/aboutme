bf67790cbde1a5ef94bb58c4ca48dab728aefb28
Simon <simon@jampot.design>
2016-07-22 21:52:53 +0000

    shatter installed but not defined

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -4,15 +4,7 @@ var webpack = require('webpack');
 
 var packageData = require('./package.json');
 
-var minify = process.argv.indexOf('--minify') != -1;
-
 var filename = [packageData.name, packageData.version, 'js'];
-var plugins = [];
-
-if (minify) {
-    filename.splice(filename.length - 1, 0, 'min');
-    plugins.push(new webpack.optimize.UglifyJsPlugin());
-}
 
 module.exports = {
     entry: path.resolve(__dirname, packageData.main),
@@ -20,18 +12,5 @@ module.exports = {
         path: path.resolve(__dirname, 'build'),
         filename: filename.join('.'),
     },
-    devtool: 'source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015']
-                }
-            }
-        ]
-    },
-    plugins: plugins
+    devtool: 'source-map'
 }
